ca7cc4f64cf116b19aa719395f37b8723250483b
Sean Newman <sxn6296@rit.edu>
2018-08-29 14:36:16 -0400

    Add decorators for argument- and code-checking
    These decorators will make it way, way easier to check the email code
    and any required arguments.

--- portal/account.py
+++ portal/account.py
@@ -135,6 +135,59 @@ def create():
         else:
             return redirect(url_for('account.change-unsuccessful.html'))
 
+    # Serve UI
+
+
+##
+#   Decorators
+##
+class required_args(object):
+    """
+    Checks all requests on the decorated route for the specified arguments.
+    If a request does not contain all required arguments, a 400 error is
+    returned.
+    """
+    def __init__(self, post_args=None, get_args=None):
+        self.post_args = post_args
+        self.get_args = get_args
+
+    def __call__(self, route):
+        @functools.wraps(route)
+        def wrapped_route(*args, **kwargs):
+            if request.method == 'GET' and isinstance(self.get_args, list):
+                for key in self.get_args:
+                    if key not in request.args:
+                        abort(400)
+
+            elif request.method == 'POST' and isinstance(self.port_args, list):
+                for key in self.post_args:
+                    if key not in request.args:
+                        abort(400)
+
+            route(*args, **kwargs)
+        return wrapped_route
+
+
+class requires_code(object):
+    """
+    Checks the email code for all requests on the decorated route for validity.
+    If the code is not valid, a 403 error is returned.
+
+    This decorator should be used after (under) the "required_args" decorator.
+    """
+    def __init__(self, methods=['POST']):
+        self.methods = methods
+
+    def __call__(self, route):
+        @functools.wraps(route)
+        def wrapped_route(*args, **kwargs):
+            if request.method in self.methods:
+                if not check_code(request.args['code']):
+                    abort(403)
+
+            route(args, kwargs)
+        return wrapped_route
+
 
 ##
 #   Helper functions
