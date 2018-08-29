808f832007fc8bb34a59bc1a19d42ff8d834202c
Sean Newman <sxn6296@rit.edu>
2018-08-29 10:12:16 -0400

    Add temporary webcommander configs

--- portal/__init__.py
+++ portal/__init__.py
@@ -8,7 +8,9 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'portal.db')
+        DATABASE=os.path.join(app.instance_path, 'portal.db'),
+        WEBCMD_HOST='localhost'
+        WEBCMD_PORT='5000'
     )
 
     if test_config is None:
