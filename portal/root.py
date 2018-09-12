e636ea9eba57824538d473e2b6dbbddaa469963a
Sean Newman <sxn6296@rit.edu>
2018-09-12 18:14:39 -0400

    Refactor /email responses

--- portal/root.py
+++ portal/root.py
@@ -29,43 +29,48 @@ bp = Blueprint('root', __name__)
 def email():
     # Make sure it's an RIT email
     if re.search(r'@((mail|g)\.)?rit\.edu$', request.form['email']) is None:
-        return jsonify({'status': 'please use an RIT email'}), 400
+        return jsonify({'status': 'Please enter an RIT email.'}), 400
 
     # Create new code
     code = new_code(request.form['email'])
 
     # Send correct email for the request type
     if request.form['type'] == 'account.register':
-        send_email(
-            request.form['email'],
-            'RITSEC Account Registration Link',
-            make_email_body(
-                'create a new RITSEC account for this email address',
-                'create your new RITSEC account',
-                code,
-                '/account/register'
-            )
+        subject = 'RITSEC Account Registration Link'
+        body = make_email_body(
+            'create a new RITSEC account for this email address',
+            'create your new RITSEC account',
+            code,
+            '/account/register'
         )
-        return jsonify({'status': 'email sent'})
 
     elif request.form['type'] == 'account.change-password':
-        send_email(
-            request.form['email'],
-            'RITSEC Account Password Reset Link',
-            make_email_body(
-                'reset the password for the RITSEC account associated with '
-                'this email address',
-                'change your account\'s password',
-                code,
-                '/account/change-password'
-            )
+        subject = 'RITSEC Account Password Reset Link'
+        body = make_email_body(
+            'reset the password for the RITSEC account associated with this '
+            'email address',
+            'change your account\'s password',
+            code,
+            '/account/change-password'
         )
-        return jsonify({'status': 'email sent'})
 
     else:
         # If the type argument is not one of the above, we don't know what to
         # do with it.
-        return jsonify({'status': 'unknown type provided'}), 400
+        return jsonify({'status': 'unknown type'}), 400
+
+    success = send_email(
+        request.form['email'],
+        subject,
+        body
+    )
+    if success:
+        return jsonify({'status': 'Email sent!'})
+    else:
+        return jsonify({
+            'status': 'There was an error sending the email.  Please contact '
+                      'the administrator at ritsecclub@gmail.com.'
+        }), 400
 
 
 @bp.route('/', methods=['GET'])
