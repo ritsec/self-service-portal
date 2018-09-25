e68d1112d05fcf29de0a56c7d72be7e6b5e948d6
Sean Newman <sxn6296@rit.edu>
2018-09-25 01:56:29 -0400

    Update email functionality

--- portal-app/portal/root.py
+++ portal-app/portal/root.py
@@ -31,28 +31,52 @@ def email():
     if re.search(r'@((mail|g)\.)?rit\.edu$', request.form['email']) is None:
         return jsonify({'status': 'Please enter an RIT email.'}), 400
 
+    # Get user ID
+    user_id = get_user_id(request.form['email'])
+
     # Create new code
-    code = new_code(request.form['email'])
+    # code = new_code(request.form['email'])
 
     # Send correct email for the request type
     if request.form['type'] == 'account.register':
-        subject = 'RITSEC Account Registration Link'
-        body = make_email_body(
-            'create a new RITSEC account for this email address',
-            'create your new RITSEC account',
-            code,
-            '/account/register'
-        )
+        first = 'create a new RITSEC GitLab account for this email address'
+        if user_id is None:
+            subject = 'RITSEC GitLab Account Registration Link'
+            body = make_email_body(
+                first,
+                'create your new RITSEC GitLab account',
+                code,
+                '/account/register'
+            )
+        else:
+            subject = 'RITSEC GitLab Account Suspicious Activity'
+            body = make_bad_email_body(
+                first,
+                'please reset your password instead of trying to create a new '
+                'account.  You can only have one GitLab account per email '
+                'address',
+            )
 
     elif request.form['type'] == 'account.change-password':
-        subject = 'RITSEC Account Password Reset Link'
-        body = make_email_body(
-            'reset the password for the RITSEC account associated with this '
-            'email address',
-            'change your account\'s password',
-            code,
-            '/account/change-password'
+        first = (
+            'reset the password for the RITSEC GitLab account associated this '
+            'email address'
         )
+        if user_id is not None:
+            subject = 'RITSEC GitLab Account Password Reset Link'
+            body = make_email_body(
+                first,
+                'change your account\'s password',
+                code,
+                '/account/change-password'
+            )
+        else:
+            subject = 'RITSEC GitLab Account Suspicious Activity'
+            body = make_bad_email_body(
+                first,
+                'please create a new account instead of trying to reset your '
+                'password.  There is no account to reset a password for',
+            )
 
     else:
         # If the type argument is not one of the above, we don't know what to
